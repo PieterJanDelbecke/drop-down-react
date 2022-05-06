af6f7185c859525c702884b0cc185be55f5c6a7d
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-05-06 14:18:29 +1000

    review of dropdownList

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { CurrencyDropdown, DropdownContainer } from './dropdownStyles'
 import Flag from 'react-flagkit'
+import { Text } from '../../globalStyles'
+import { IconContext } from 'react-icons'
+import { AiOutlineCaretDown } from 'react-icons/ai'
 
 const Dropdown = () => {
   return (
     <DropdownContainer>
         <CurrencyDropdown>
-            <Flag size='50' country="US"/>
+            <Flag size='32' country="US"/>
+            <Text color='#f4f4f4'>USD</Text>
+            <IconContext.Provider value={{size:'1.3rem', color:'#dfdfdf'}}>
+                <AiOutlineCaretDown />
+            </IconContext.Provider>
         </CurrencyDropdown>
     </DropdownContainer>
   )
